c0cb565df757b5338b9aa743179fccb7339ce21e
Ewelina Momot <ewemom@st.amu.edu.pl>
2026-05-25 11:44:17 +0200

    adding product service

---
+++ client/src/services/productService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { mapToProductListDto, ProductDto } from '@/DTOs/ProductDTO';
+
+const BASE_URL='https://localhost:7154/api/products';
+
+export const productService={
+
+    async getProduct(): Promise<ProductDto[]>  {
+  try {
+    const response = await axios.get(BASE_URL)
+    const rawProducts = response.data.products;
+    return mapToProductListDto(rawProducts)
+  } catch (error) {
+    console.error("Błąd w serwisie podczas pobierania produktów:", error);
+      throw error;  }
+}
+}
